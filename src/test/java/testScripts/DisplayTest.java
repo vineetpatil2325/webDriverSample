f7ed8bce43a0004f682f928f6f18fc6c6d0b9503
vineetpatil2325 <vineetpatil2325@gmail.com>
2024-12-11 11:29:07 +0530

    Update Chnages

--- src/test/java/testScripts/DisplayTest.java
+++ src/test/java/testScripts/DisplayTest.java
@@ -1,7 +1,9 @@
 package testScripts;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
@@ -15,19 +17,22 @@ import org.openqa.selenium.TakesScreenshot;
 
 public class DisplayTest {
   @Test
-  public void DisplayElemTest() {
+  public void DisplayElemTest() throws IOException {
 	  WebDriver driver = new ChromeDriver();
 	  driver.manage().window().maximize();
 	  driver.get("https://automationbookstore.dev/");
 	  
 	  TakesScreenshot screen=(TakesScreenshot)driver;
 	  File src =screen.getScreenshotAs(OutputType.FILE);
-	  	String path =System.getProperty("user.dir");
+	  	String path =System.getProperty("user.dir")
+	  			+"/screenshots"+System.currentTimeMillis()+".png";
+	  	FileUtils.copyFile(src, new File(path));
 	  
-	  driver.findElement(By.id("searchbar")).sendKeys("test");
+	  driver.findElement(By.id("searchbar")).sendKeys("Test");
 	  WebElement closeIcon =driver.findElement(
 			  By.cssSelector("a[title ='Clear text]"));
-	  if(closeIcon.isDisplayed()) {
+	  if(closeIcon.isDisplayed()) 
+	  {
 		  closeIcon.click();
 	  }
 	  
